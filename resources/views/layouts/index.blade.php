39cf60d0f8fbf948a1f714860c3bb60bf3b9d8fc
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-09-08 10:11:11 +0700

    adding library

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Angular-Laravel</title>
+    <base href="{{asset('')}}">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="angular/angular.min.js"></script>
+    <script src="angular/demo.js"></script>
+</head>
+
+<body>
+
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,6 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.index');
 });
+ Route::resource('users','UserController');
