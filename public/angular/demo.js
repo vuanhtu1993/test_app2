b0519b09e58d1e85b6d75348f1ba5986f138ab9e
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-09-11 17:01:04 +0700

    purpose of Directive ngFile to capture any changes that occur in the input element how get the value ??? use $parse service Angular $file where save input file

--- public/angular/demo.js
+++ public/angular/demo.js
@@ -2,10 +2,53 @@ var myApp = angular.module('myApp',[],function ($interpolateProvider) {
     $interpolateProvider.startSymbol('<%');
     $interpolateProvider.endSymbol('%>');
 });
-myApp.controller('DemoController',function () {
-   var demo = this;
+//Directive
+myApp.directive('ngFiles', ['$parse', function ($parse) {
 
-   demo.modal = function () {
-       $('#myModal').modal('show');
-   }
+    function fn_link(scope, element, attrs) {
+        var onChange = $parse(attrs.ngFiles);
+        element.on('change', function (event) {
+            onChange(scope, { $files: event.target.files });
+        });
+    };
+
+    return {
+        link: fn_link
+    }
+} ]);
+
+
+
+//Controller
+myApp.controller('DemoController', function ($scope, $http) {
+
+    $scope.modal = function () {
+      $('#myModal').modal('show');
+    };
+
+    var formdata = new FormData();
+    $scope.getTheFiles = function ($files) {
+        console.log($files); // get file immediately after input
+    };
+
+    // NOW UPLOAD THE FILES.
+    $scope.uploadFiles = function () {
+
+        var request = {
+            method: 'POST',
+            url: 'http://localhost/test_app2/public/users',
+            data: formdata,
+            headers: {
+                'Content-Type': undefined
+            }
+        };
+
+        // SEND THE FILES.
+        $http(request)
+            .success(function (d) {
+                alert(d);
+            })
+            .error(function () {
+            });
+    }
 });
