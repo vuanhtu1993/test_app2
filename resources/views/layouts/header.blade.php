a6a4558a35e08a46785a40c8246a11695f388315
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-09-16 12:02:05 +0700

    do completely interface

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -27,18 +27,8 @@
             </div>
             {{--end search--}}
             <ul class="nav navbar-nav navbar-right">
-                <li><a style="color: rgb(89, 205, 227); font-weight: bold" href="#" ng-click="modal()">Link</a></li>
-                <li class="dropdown">
-                    <a style="color: orange; font-weight: bold" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                       aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
-                </li>
+                <li><a style="color: rgb(89, 205, 227); font-weight: bold" href="#" ng-click="modal()">Add +</a></li>
+
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,6 +7,7 @@
     <base href="{{asset('')}}">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="fonts/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
@@ -25,15 +26,21 @@
                         <thead>
                         <tr>
                             <th>ID</th>
-                            <th><a href="#" ng-click="sortType = 'name'; sortReverse = !sortReverse">Name </a></th>
-                            <th><a href="#" ng-click="sortType = 'address'; sortReverse = !sortReverse">Address </a></th>
-                            <th><a href="#" ng-click="sortType = 'age'; sortReverse = !sortReverse">Age </a></th>
+                            <th><a href="#" ng-click="sortType = 'name'; sortReverse = !sortReverse">Name
+                                    <span ng-show="sortType == 'name'" class="fa fa-caret-down"></span>
+                                </a></th>
+                            <th><a href="#" ng-click="sortType = 'address'; sortReverse = !sortReverse">Address
+                                    <span ng-show="sortType == 'address'" class="fa fa-caret-down"></span>
+                                </a></th>
+                            <th><a href="#" ng-click="sortType = 'age'; sortReverse = !sortReverse">Age
+                                    <span ng-show="sortType == 'age'" class="fa fa-caret-down"></span>
+                                </a></th>
                             <th style="text-align: center">Image</th>
                             <th>Edit/Delete</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <tr ng-repeat="user in users | orderBy:sortType:sortReverse ">
+                        <tr ng-repeat="user in users | orderBy:sortType:sortReverse|filter:search">
                             <th style=""><% user.id %></th>
                             <td style=""><div class="layout_name" ><b>Name : </b> <% user.name %>  </div></td>
                             <td style="" >
@@ -43,9 +50,9 @@
                             <td style="width: 20%"><img style="width: 100%;" src="uploads/<% user.link %>" alt=""></td>
 
                             <td style="width: 10%">
-                                <div ><button id="button_edit" ng-click="modal_edit(user)"><img style="width: 40%" src="images/edit.jpg" alt=""></button></div>
-                                <div style="height: 40px"></div>
-                                <div ><button id="button_edit" ng-click="delete(user)" ><img style="width: 50%" src="images/delete.png" alt=""></button></div>
+                                <div ><button id="button_edit" ng-click="modal_edit(user)"><img style="width: 30%" src="images/edit.jpg" alt=""></button></div>
+                                <div style="height: 30px"></div>
+                                <div ><button id="button_edit" ng-click="delete(user)" ><img style="width: 40%" src="images/delete.png" alt=""></button></div>
                             </td>
 
                         </tr>
